eb92648f2e585402bed2b3ce9934ee8af9678a8c
Tony <tonyzoudev@gmail.com>
2024-05-05 04:05:23 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,52 @@
+let num1 = 0;
+let num2 = 0;
+let op = "";
+
+let display = document.getElementById('display');
+let buttons = document.querySelectorAll('.number');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        display.textContent += button.textContent;
+    });
+});
+
+
+
+function add(a, b)
+{
+    return a + b;
+}
+
+function subtract(a, b)
+{
+    return a - b;
+}
+
+function multiply(a, b)
+{
+    return a * b;
+}
+
+function divide(a, b)
+{
+    return a / b;
+}
+
+function operate(a, b, sym)
+{
+    switch (sym) {
+        case "+":
+            add(a, b);
+            break;
+        case "-":
+            subtract(a, b);
+            break;
+        case "*":
+            multiply(a, b);
+            break;
+        case "/":
+            divide(a, b);
+            break;
+    }
+}
