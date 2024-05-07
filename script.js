0c5abb1e8b9e2f02a523b0b67971204392b46f37
Tony <tonyzoudev@gmail.com>
2024-05-06 20:57:14 -0400

    need to fix operate func

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-let num1 = 0;
-let num2 = 0;
-let op = "";
+let num1 = '';
+let num2 = '';
+let op = '';
 
 let display = document.getElementById('display');
 let numButtons = document.querySelectorAll('.number');
 
 numButtons.forEach(button => {
     button.addEventListener('click', () => {
-        display.textContent += button.textContent;
+        if(!op)
+        {
+            num1 += button.textContent;
+            display.textContent = num1;
+        }
+        else
+        {
+            num2 += button.textContent;
+            display.textContent = num2;
+        }
     });
 });
 
@@ -15,9 +24,23 @@ let opButtons = document.querySelectorAll('.op');
 
 opButtons.forEach(button => {
     button.addEventListener('click', () => {
-        display.textContent += button.textContent;
+        if(op && num1 && num2)
+        {
+            display.textContent = operate(num1, num2, op);
+            op = button.textContent;
+        }
+        else if(op && num1)
+        {
+            op = button.textContent;
+        }
+        else
+        {
+            op = button.textContent;
+        }
     });
 });
+
+
 
 
 
@@ -43,18 +66,28 @@ function divide(a, b)
 
 function operate(a, b, sym)
 {
+    a = parseInt(a);
+    b = parseInt(b);
     switch (sym) {
         case "+":
+            display.textContent = '';
             add(a, b);
             break;
         case "-":
+            display.textContent = '';
             subtract(a, b);
             break;
         case "*":
+            display.textContent = '';
             multiply(a, b);
             break;
         case "/":
+            display.textContent = '';
             divide(a, b);
+            break;
+        default:
+            display.textContent = 'error';
             break;
     }
 }
+
